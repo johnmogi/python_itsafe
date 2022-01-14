6dffe11bfa7c2c48b92b5cc4edf5db319e627b16
johnmogi <anguru@gmail.com>
2022-01-14 16:57:46 +0700

    1

---
+++ L1/1.py
@@ -0,0 +1,13 @@
+text="*"
+print(text * 10)
+
+headline = "My name is inigo"
+print(headline[::-1])
+
+
+
+name = input('what\'s your name? ')
+wellcome = 'wellcome home, '
+print (wellcome + name)
+
+
